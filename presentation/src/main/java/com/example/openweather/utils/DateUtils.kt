9abc483cdb89a362f472cbaa0d9f1d1d28ba1ee6
Igor Levchenko <i2.levchenko@itransition.com>
2020-01-21 15:14:22 +0300

    ui improvement

--- presentation/src/main/java/com/example/openweather/utils/DateUtils.kt
+++ presentation/src/main/java/com/example/openweather/utils/DateUtils.kt
@@ -2,6 +2,7 @@ package com.example.openweather.utils
 
 import android.text.format.DateUtils
 import java.text.DateFormat
+import java.text.DateFormatSymbols
 import java.text.SimpleDateFormat
 
 object DateUtils {
@@ -12,6 +13,33 @@ object DateUtils {
 
     @JvmStatic
     fun formatToTime(date: Long): String {
-        return SimpleDateFormat("hh:mm").format(date*DateUtils.SECOND_IN_MILLIS)
+        return SimpleDateFormat("hh:mm").format(date * DateUtils.SECOND_IN_MILLIS)
+    }
+
+    @JvmStatic
+    fun myFormatToTime(date: Long): String {
+        return SimpleDateFormat(
+            "dd MMMM",
+            myDateFormatSymbols
+        ).format(date * DateUtils.SECOND_IN_MILLIS)
+    }
+
+    private val myDateFormatSymbols = object : DateFormatSymbols() {
+        override fun getMonths(): Array<String> {
+            return arrayOf(
+                "января",
+                "февраля",
+                "марта",
+                "апреля",
+                "мая",
+                "июня",
+                "июля",
+                "августа",
+                "сентября",
+                "октября",
+                "ноября",
+                "декабря"
+            )
+        }
     }
 }
